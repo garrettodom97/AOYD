b1acbd4253d35b4ca71d8b9381ea98b83d2c6454
garrettodom97 <garrettodom97@gmail.com>
2023-08-03 16:43:15 -0600

    add downfall story

--- src/pages/DownfallInfo.js
+++ src/pages/DownfallInfo.js
@@ -3,8 +3,143 @@ import "./DownfallInfo.css";
 
 function Downfall() {
   return (
-    <div className="Downfall">
-      <h2>Coming Soon</h2>
+    <div className="downfall-info">
+      <h1>Downfall</h1>
+      <h2>[Prologue]</h2>
+      <h2>
+        The intrepid crew of the Sol Techno-Conglomerate Starship, the S.T.C.S
+        Downfall—a Progressive-Class vessel—was dispatched in response to a
+        distress call from an allied world.{" "}
+      </h2>
+      <h2>[REVV]</h2>
+      <h2>
+        The S.T.C.S. Downfall jumped to FTL (Fast Than Light). While the
+        Downfall zipped through space at 5x the speed of light, the crew
+        prepared for the worst. Captain Cozza sat stoically at the center of the
+        bridge, his chin resting thoughtfully on his fist as he stared out the
+        viewscreen at the stars streaking by. The bridge crew worked diligently,
+        moving from station to station like a well-choreographed dance to
+        unheard music. Only hushed whispers and the thrum of the FLT engines
+        could be heard.
+        <br></br>“ETA thirty seconds, Captain,” Lieutenant Commander Kru, the
+        ship’s helmsman, announced.
+        <br></br>“Take us out of FTL,” Captain Cozza ordered.{" "}
+      </h2>
+      <h2>[ARTAX]</h2>
+      <h2>
+        With a bright flash of light, the Downfall emerged from FTL. Cozza
+        stood, jaw agape in unison with his crew. A black disk rimmed in
+        iridescent light the width of thousands of starships lined up
+        bow-to-stern yawned before them. In the distance, the light of the
+        system’s star began to fade as a trail of roiling yellow-orange energy
+        was drawn into the maw’s gravitational well.
+        <br></br>“We’re too late,” Lieutenant Odom, the ship’s tactical officer
+        murmured from his station, his voice full of dread as they all watched
+        the planet dead ahead break apart and stretch into the great unknown
+        extinguishing billions of lives in the matter of seconds. Their
+        horrified cries cutting through the static on an open channel.
+        <br></br>“You know,” Commander Nicholson, the ship’s chief engineer,
+        chimed in, “that reminds me of this ancient earth movie where this horse
+        gets sucked into a black—” Captain Cozza’s head snapped around as he
+        gave Nicholson a stern gaze, prompting a sheepish look from his
+        engineer. “S-sorry. Never… um…never mind, sir. It’s… it’s not
+        important.”
+        <br></br>A sudden jolt drew their attention back to the task at hand.
+        Klaxons deafened those on the bridge as the ship shuddered violently.
+        <br></br>“Report!” Cozza barked.
+        <br></br>“We’re caught in its gravity well, captain!” Nicholson shouted
+        over the alarms.
+        <br></br>“Damn!” Cozza growled. “Kru, full reverse!”
+        <br></br>“Aye, Captain!” Kru responded. “Full reverse!”
+        <br></br>The ship bucked hard to the left and then to the right as it
+        struggled to widen the gap between them and the most powerful force in
+        the known universe.
+        <br></br>“We’re not breaking free!” Kru yelled over the rumble.
+        <br></br>“Joe!” Cozza snapped, looking back at his engineer. “We need
+        more power!”
+        <br></br>“I’m giving you all she’s got, Captain!” Nicholson responded.
+      </h2>
+      <h2>[TEMPORAL CRISIS]</h2>
+      <h2>
+        “Picking up an odd time signature from the navigational sensors!” Kru’s
+        voice sounded off from the helm slower and deeper than it should have
+        been.
+        <br></br>“Joe, what the hell’s happening?” Cozza demanded.
+        <br></br>“As we draw nearer to the event horizon, time dilates! Time is
+        moving slower at the front of the ship. Before too long, the time
+        dilation will cause stress fractures on the hull! She can’t handle
+        existing in multiple times simultaneously! Structural integrity will
+        collapse before we get sucked in!”
+        <br></br>“What if we go to full speed? Can we punch through?” Cozza
+        offered.
+        <br></br>“No! The closer we get, the more time dilates, we’ll be torn
+        apart even quicker, and slower at the same time.” Nicholson responded.
+        <br></br>“There has to be something we can do!” Odom replied, slamming
+        his fist down on his tactical console in frustration.
+        <br></br>“I cannot change the laws of physics!” Nicholson retorted.{" "}
+      </h2>
+      <h2>[CELESTIAL ABYSS] </h2>
+      <h2>
+        Looking out the viewscreen, Captain Cozza watched as the bow of his ship
+        started to break apart in slow-motion. Sparks began to fly across the
+        bridge as bulkheads gave to the gravitational/temporal stress. Dread hit
+        the bottom of his stomach like a spoonful of thick mashed potatoes. He
+        stared intently into the black disk of oblivion. He could almost hear it
+        speaking to him.
+        <br></br>I am your end. I am the devourer of space and time itself. The
+        author of galactic destruction… I am… the Author of Your Downfall…
+        <br></br>“Well, gentlemen…” He breathed in deeply, pride filling his
+        chest as his eyes lit upon each member his crew, his friends, his
+        family. “It’ been an—”{" "}
+      </h2>
+      <h2>[THE ANOMALY] </h2>
+      <h2>
+        “Captain!” Nicholson yelped, cutting off his commanding officer’s final
+        words. “Sensors are picking up something massive at the singularity’s
+        center! I’m picking up gravimetric waves bleeding off it! I theorize
+        that whatever that is, it’s at the heart of what’s happening here! If
+        these readings are correct, that anomaly is the cause!”
+        <br></br>Cozza stood abruptly, straightening his uniform. “Can you be
+        more specific, Joe? Is it a ship? A station? A weapon?”
+        <br></br>“Unfortunately, I don’ know,” Nicholson said before a dreadful
+        gaze met his captain’s eyes. “With all the interference caused by the
+        blackhole, we would have to get closer.”
+        <br></br>“Well, that thing isn’t exactly giving us much of a choice,”
+        Kru offered.
+        <br></br>“Agreed,” Cozza said with a nod. He then turned to tactical.
+        “Gary! G-gar…Garman? Gretta? G-Man?”
+        <br></br>“Garrett, sir!”
+        <br></br>“Right!” Garrett was the newest member of the crew. “Garrett,
+        ready weapons! Maybe we can disable or destroy whatever that is in
+        there. I want a firing solution the moment we’re in range!”
+        <br></br>“Aye, Captain!” Odom responded, his fingers flying over the
+        console like a pianist to 88 keys.
+        <br></br>Stress fractures began to spread like deadly spiderwebs across
+        the viewscreen as hull panels ripped from the bow of the ship and pelted
+        the bridge.
+        <br></br>Warning. Structural Integrity at 74 percent and falling. The
+        computer’s voice cut through the chaos.
+        <br></br>“How much closer?” Cozza asked his engineer, beads of sweat
+        forming on his brow.
+        <br></br>“Another two thousand kilometers!” Nicholson replied.
+        <br></br>Warning. Structural Integrity at 68 percent and falling.
+        <br></br>“Kru,” Cozza barked. “ETA to sensor range?”
+        <br></br>“Twenty seconds, Captain!”
+        <br></br>That twenty seconds lasted an eternity. Entire sections of the
+        ship were venting atmosphere—and crew—into space as the temporal and
+        gravimetric stress increased.
+        <br></br>Warning. Structural Integrity at 64 percent and falling.
+        <br></br>A soft alarm sounded from engineering. “Got it!” Nicholson
+        shouted triumphantly. “Putting it up on the viewscreen now!”
+        <br></br>Warning. Structural Integrity at 60 percent and falling.
+        <br></br>Captain Cozza stared at the anomaly, eyes wide, jaw agape. “My
+        god.” he gasped. “Is that…?”
+        <br></br>His eyes not leaving the viewscreen, Nicholson replied. “I’m
+        afraid it is, Captain.”
+        <br></br>“How’s that possible?” Kru asked from his station.
+        <br></br>“I, I don’t know… but sensors confirm. It’s…”{" "}
+      </h2>
+      <h2>***To be continued on the next album***</h2>
     </div>
   );
 }
