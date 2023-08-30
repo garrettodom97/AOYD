ac0d144bc63c40a08a99e98d9e39f5dfb8f0ee65
garrettodom97 <garrettodom97@gmail.com>
2023-08-30 16:57:48 -0600

    update upcoming shows

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -5,7 +5,6 @@ function Schedule() {
   return (
     <div className="schedule">
       <h2>Upcoming Shows</h2>
-      <img src="./images/lehnen.png" width="500px" height="500px" />
       <img src="./images/release-show.png" width="500px" height="500px" />
     </div>
   );
