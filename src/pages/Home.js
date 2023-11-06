ef79966600f5df358169580d59042ac4d9e0e587
garrettodom97 <garrettodom97@gmail.com>
2023-11-06 14:25:41 -0700

    remove lazy load

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,19 @@
 import "./Home.css";
 import Header from "../components/Header";
+import SpotifyEmbed from "../components/SpotifyEmbed";
 import Schedule from "../components/Schedule";
 import Footer from "../components/Footer";
 import Downfall from "../components/Downfall";
 import Tools from "../components/Tools";
 import Album from "../components/Album";
 import Disclaimer from "../components/Disclaimer";
-import React, { Suspense } from "react";
-const SpotifyEmbed = React.lazy(() => import("../components/SpotifyEmbed"));
 
 function Home() {
   return (
     <div className="Home">
       <Downfall />
       <Album />
-      <Suspense fallback={<div>Loading...</div>}>
-        <SpotifyEmbed />
-      </Suspense>
+      <SpotifyEmbed />
       <Schedule />
       <Tools />
       <Footer />
