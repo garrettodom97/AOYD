de63b584e77db8dc014b648c057d635c760498b8
garrettodom97 <garrettodom97@gmail.com>
2023-11-06 13:51:51 -0700

    add flyers for upcoming shows and fix mobile icons

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -5,7 +5,12 @@ function Schedule() {
   return (
     <div className="schedule">
       <h2>Upcoming Shows</h2>
-      <img src="./images/AOYD_WY.png" width="500px" height="500px" />
+      <img
+        src="./images/battle_of_the_bands.png"
+        width="500px"
+        height="500px"
+      />
+      <img src="./images/harpoon_the_whale.png" width="500px" height="500px" />
     </div>
   );
 }
