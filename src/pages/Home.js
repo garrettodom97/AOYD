b3ff36a2c29b078d53c55c3aa66b442b984a41fc
garrettodom97 <garrettodom97@gmail.com>
2023-11-06 14:14:43 -0700

    lazy load spotify and youtube players

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
 import "./Home.css";
 import Header from "../components/Header";
-import SpotifyEmbed from "../components/SpotifyEmbed";
 import Schedule from "../components/Schedule";
 import Footer from "../components/Footer";
 import Downfall from "../components/Downfall";
 import Tools from "../components/Tools";
 import Album from "../components/Album";
 import Disclaimer from "../components/Disclaimer";
+import React, { Suspense } from "react";
+const SpotifyEmbed = React.lazy(() => import("../components/SpotifyEmbed"));
 
 function Home() {
   return (
     <div className="Home">
       <Downfall />
       <Album />
-      <SpotifyEmbed />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SpotifyEmbed />
+      </Suspense>
       <Schedule />
       <Tools />
       <Footer />
