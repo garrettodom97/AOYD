c4b7071c5d20ea5f1848ce8cea2aefbffce46c21
garrettodom97 <garrettodom97@gmail.com>
2024-02-27 10:29:58 -0700

    update upcoming shows section

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -5,12 +5,7 @@ function Schedule() {
   return (
     <div className="schedule">
       <h2>Upcoming Shows</h2>
-      <img
-        src="./images/battle_of_the_bands.png"
-        width="500px"
-        height="500px"
-      />
-      <img src="./images/harpoon_the_whale.png" width="500px" height="500px" />
+      <h3>Come back soon for info about our next show!</h3>
     </div>
   );
 }
